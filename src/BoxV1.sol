79d2ee8418576824037c3ae6ce1db68d2b4f91be
supersakana <zacwilliamson@icloud.com>
2025-03-03 17:11:35 -0600

    installed openzeppelin upgradable contracts and set up base contracts

---
+++ src/BoxV1.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+// contains all of the upgradeabillity logic
+contract BoxV1 {
+    uint256 internal number;
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+}
---
+++ src/BoxV2.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+contract BoxV2 {
+    uint256 internal number;
+
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
+
+    function getNumber() external view returns (uint256) {
+        return number;
+    }
+
+    function version() external pure returns (uint256) {
+        return 2;
+    }
+}

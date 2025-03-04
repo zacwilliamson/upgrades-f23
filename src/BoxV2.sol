cb0fa382d8ad47190acba50ef10ea1a389373cd2
supersakana <zacwilliamson@icloud.com>
2025-03-03 19:31:11 -0600

    made contracts upgradeable

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -2,8 +2,22 @@
 
 pragma solidity ^0.8.18;
 
-contract BoxV2 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init();
+        __UUPSUpgradeable_init();
+    }
 
     function setNumber(uint256 _number) external {
         number = _number;
@@ -16,4 +30,6 @@ contract BoxV2 {
     function version() external pure returns (uint256) {
         return 2;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
